e86797f4b1bae6f0ee3ee16d33bc1757e009207c
Tyler Long <tyler4long@gmail.com>
2022-12-07 10:43:12 -0800

    Try to make subscription work

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (code === null) {
   const someBytes = randomBytes(32);
   const codeVerifier = buffer2string(someBytes);
   localforage.setItem('codeVerifier', codeVerifier).then(() => {
-    // login
+    // generate login link
     const loginUrl = platform.loginUrl({
       redirectUri,
       code_challenge_method: 'S256',
@@ -29,12 +29,38 @@ if (code === null) {
   // exchange code for token
   localforage.getItem('codeVerifier', (err, codeVerifier) => {
     platform
-    .login({
-      code, 
-      redirect_uri: redirectUri, 
-      code_verifier: codeVerifier
-    })
-    .then(r => r.json())
-    .then(token => document.write(JSON.stringify(token, null, 2)));
+      .login({
+        code, 
+        redirect_uri: redirectUri, 
+        code_verifier: codeVerifier
+      })
+      .then(r => r.json())
+      .then(token => document.write('<pre>' + JSON.stringify(token, null, 2) + '</pre>'));
+
+  // make API call
+  platform.get('/restapi/v1.0/account/~/extension/~')
+   .then(r => r.json())
+   .then(ext => {
+      document.write(`<p>Your extension ID is ${ext.id}</p>`);
+      // subscription
+      var subscriptions = new RingCentral.Subscriptions({
+        sdk: rc
+      });
+      const subscription = subscriptions.createSubscription();
+      subscription.on(subscription.events.notification, (evt) => {
+        console.log(JSON.stringify(evt, null, 2));
+      });
+      subscription
+        .setEventFilters(['/restapi/v1.0/account/~/extension/~/message-store'])
+        .register()
+        .then(() => {
+          // trigger a message
+          platform.post('/restapi/v1.0/account/~/extension/~/company-pager', {
+            from: {extensionId: ext.id},
+            to: [{extensionId: ext.id}],
+            text: 'Hello world!',
+          });
+        });
+    });
   });
 }
